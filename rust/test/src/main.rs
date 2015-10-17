8e8b6391a5547bee82ad46f10995a1a2008cd28e
Andre Segura <andrensegura@gmail.com>
2015-10-17 17:45:39 -0400

    testies

--- rust/test/src/main.rs
+++ rust/test/src/main.rs
@@ -1,31 +1,124 @@
+extern crate lib_andre;
+extern crate walkdir;
+
+use walkdir::WalkDir;
+use lib_andre::os::is_valid_user;
+//use lib_andre::io::print_file;
+//use std::process::Command;
 use std::error::Error;
+use std::env;
 use std::path::PathBuf;
-use std::fs::File;
-use std::io::prelude::*;
+
+
+struct Flags {
+    interactive: bool,
+    permissions: bool,
+    suspended: bool,
+    htaccess: bool,
+    inodes: bool,
+
+}
 
 
 fn main() {
-    let user = "employee";
-    
-    let mut suspend_file_path = PathBuf::from("/var/cpanel/suspended/");
-    suspend_file_path.push(user);
-    
-    match print_file(suspend_file_path.to_str().unwrap()) {
-        //Ok(s) => println!("{} is SUSPENDED: {}", user, s),
-        Ok(s) => println!("{} is SUSPENDED.", user),
-        Err(_) => println!("{} is not suspended.", user),
+    try_main().unwrap();
+}
+
+fn try_main() -> Result<(), Box<Error>> {
+
+    let args: Vec<_> = env::args().collect();
+    let mut argument_flags = Flags { interactive: false,
+                                     permissions: false,
+                                     suspended: false,
+                                     htaccess: false,
+                                     inodes: false };
+
+    //no args
+    if args.len() < 2 {
+        print_usage();
+        return Ok(())
     }
-    
-    
 
+    //check the first argument. if it's not a user, print usage > exit.
+    let user = args[1].clone();
+
+    println!("-- Checking user ...");
+    if is_valid_user(user.as_ref()) {
+        println!("-- {} is a valid user.", user);
+    } else {
+        println!("-- {} is an INVALID user.", user);
+        return Ok(())
+    }
+
+    //now grabs the rest of the arguments.
+    if args.contains(&"-i".to_string()) {
+        argument_flags.interactive = true;
+        println!("-- INTERACTIVE flag set.");
+    }
+    if args.contains(&"-n".to_string()) {
+        argument_flags.inodes = true;
+        //run the function for this.
+        try!(check_inodes(user.as_ref(), argument_flags.interactive));
+    }
+
+    Ok(())
 }
 
+fn check_inodes(user: &str, interactive: bool) -> Result<(), Box<Error>>{
+    println!("-- INODES flag set. user:{} inter:{}", user, interactive);
+    //let path = "/home/".to_string() + user + "/";
+    //println!("Files: {}", WalkDir::new(path).into_iter().count());
+    let homedir = WalkDir::new("/home/".to_string() + user + "/");
 
-pub fn print_file(file_name: &str) -> Result<String, Box<Error>> {
-    let mut f = try!(File::open(file_name));
-    let mut s = String::new();
-    try!(f.read_to_string(&mut s));
+    for entry in homedir {
+        let entry = entry.unwrap();
+        println!("{}", entry.path().display());
+    }
+    println!("Files: {}", homedir.into_iter().count());
 
-    Ok(s)
+    Ok(())
 }
 
+//fn get_command_output(command: String) -> String {
+//    let output = Command::new("sh")
+//                            .arg("-c")
+//                            .arg(command)
+//                            .output()
+//                            .unwrap_or_else(|e| { panic!("failed to execute process: {}", e) });
+//    String::from_utf8_lossy(&output.stdout).to_string()
+//}
+
+
+
+fn print_usage() {
+    let usage = r#"
+-------------------------------------------------------------------------------
+Certified Hosting -- acctDiag -- Usage
+This tool is used to diagnose common issues with cPanel users. The program will 
+make no changes to accounts (unless using interactive mode, be careful!), but
+will report issues that it finds.
+
+This is still a work in progress. Issues may be found, features may be added.
+Any bugs, issues, or feature requests can be sent to andre@certifiedhosting.com.
+
+>>SYNTAX:
+[~]# acctDiag CPUSER <-i> <-n> <-p> <-h> <-s>
+*************
+CPUSER          :       Must be a valid cPanel user. MUST BE FIRST ARGUMENT.
+-i              :       INTERACTIVE flag. Setting this flag will allow you
+                        make runtime decisions. Try it out. MUST BE 2nd ARG.
+*************
+-n              :       Check the amount of iNodes the account has.
+-p              :       Check file/directory Permissions.
+-h              :       Check for issues with a .Htaccess file.
+-s              :       Check if the account is Suspended.
+*************
+CPUSER must come first, -i must come second (optional).
+The rest may be in any order.
+You do not need to include any flags. By default, the program will have all
+flags set; setting one manually turns all others off.
+-Andre
+-------------------------------------------------------------------------------
+"#;
+    println!("{}", usage);
+}
---
+++ rust/test/target/debug/test.d
@@ -0,0 +1,4 @@
+/home/andre/projects/rust/test/target/debug/test: src/main.rs
+
+/home/andre/projects/rust/test/target/debug/test.d: src/main.rs
+
