03a49a0d18817cdfc42824c4547620d5ec168be7
Andre Segura <andrensegura@gmail.com>
2015-10-27 12:15:21 -0400

    THREADING SUPPORT. YEAH. v1.2.0

--- rust/acctdiag/src/main.rs
+++ rust/acctdiag/src/main.rs
@@ -7,7 +7,8 @@ use lib_andre::os::is_valid_user;
 use lib_andre::io::{print_file, prompt};
 //use std::process::Command;
 use std::error::Error;
-use std::env;
+use std::{env, thread};
+use std::sync::Arc;
 use std::os::unix::fs::PermissionsExt;
 use std::path::{PathBuf, Path};
 use std::fs::{metadata, File};
@@ -15,14 +16,10 @@ use std::io::prelude::*;
 use regex::Regex;
 
 
-struct Flags {
-    interactive: bool,
-    permissions: bool,
-    suspended: bool,
-    htaccess: bool,
-    inodes: bool,
-
-}
+const RED: &'static str = "\u{1B}[0;31m";
+const GREEN: &'static str = "\u{1B}[0;32m";
+const BLUE: &'static str = "\u{1B}[0;34m";
+const NC: &'static str = "\u{1B}[0m";
 
 
 fn main() {
@@ -31,12 +28,14 @@ fn main() {
 
 fn try_main() -> Result<(), Box<Error>> {
 
+
     let args: Vec<_> = env::args().collect();
-    let mut argument_flags = Flags { interactive: false,
-                                     permissions: false,
-                                     suspended: false,
-                                     htaccess: false,
-                                     inodes: false };
+
+    let mut interactive: bool = false;
+    let mut permissions: bool = false;
+    let mut suspended:   bool = false;
+    let mut htaccess:    bool = false;
+    let mut inodes:      bool = false;
 
     //no args
     if args.len() < 2 {
@@ -44,54 +43,98 @@ fn try_main() -> Result<(), Box<Error>> {
         return Ok(())
     }
 
-    //check the first argument. if it's not a user, print usage then exit.
-    let user = args[1].clone();
+    let mut children = vec![];
 
-    println!("-- Checking user ...");
+    println!("{}Account Diagnostics Tool v1.2.0 -- Andre Segura{}", BLUE, NC);
+
+    //check the first argument. if it's not a user, print usage then exit.
+    let user = Arc::new(args[1].clone());
+
+    println!("-- {}Checking user{}...", BLUE, NC);
     if is_valid_user(user.as_ref()) {
-        println!("-- {} is a valid user.", user);
+        println!("    {} is a valid user.", user);
     } else {
-        println!("-- {} is an INVALID user.", user);
+        println!("    {} is an {}INVALID{} user.", user, RED, NC);
         return Ok(())
     }
 
     //check args and run their functions if given.
     if args.contains(&"-i".to_string()) {
-        argument_flags.interactive = true;
+        interactive = true;
         println!("-- Flag '-i': Running in INTERACTIVE mode.");
     }
     if args.contains(&"-s".to_string()) {
-        argument_flags.suspended = true;
-        try!(check_suspension(user.as_ref(), argument_flags.interactive));
+        suspended = true;
+        try!(check_suspension(user.as_ref()));
     }
     if args.contains(&"-n".to_string()) {
-        argument_flags.inodes = true;
-        try!(check_inodes(user.as_ref(), argument_flags.interactive));
+        inodes = true;
+        let user = user.clone(); //increments the Arc counter.
+        if interactive {
+            try!(check_inodes(user.as_ref(), true));
+        }else{
+            children.push(thread::spawn(move || {
+                check_inodes(user.as_ref(), false).unwrap();
+            }));
+        }
     }
     if args.contains(&"-p".to_string()) {
-        argument_flags.permissions = true;
-        try!(check_file_permissions(user.as_ref(), argument_flags.interactive));
+        permissions = true;
+        let user = user.clone(); //increments the Arc counter.
+        if interactive {
+            try!(check_file_permissions(user.as_ref(), true));
+        }else{
+            children.push(thread::spawn(move || {
+                check_file_permissions(user.as_ref(), false).unwrap();
+            }));
+        }
     }
     if args.contains(&"-h".to_string()) {
-        argument_flags.htaccess = true;
-        try!(check_htaccess_files(user.as_ref(), argument_flags.interactive));
+        htaccess = true;
+        let user = user.clone(); //increments the Arc counter.
+        if interactive {
+            try!(check_htaccess_files(user.as_ref(), interactive));
+        }else{
+            children.push(thread::spawn(move || {
+                check_htaccess_files(user.as_ref(), interactive).unwrap();
+            }));
+        }
     }
 
     //user given, but no function arguments.
     //I originally wrote an if statement inside of this one to check the interactive
     //flag so it would decide wether to run this with/without interactive, but both
     //the with and without blocks ended up looking the same. Duh.
-    if !(  argument_flags.suspended 
-         | argument_flags.inodes 
-         | argument_flags.permissions
-         | argument_flags.htaccess ) {
-        //no functions, but interactive. run all with interactive
-        try!(check_suspension(user.as_ref(), argument_flags.interactive));
-        try!(check_inodes(user.as_ref(), argument_flags.interactive));
-        try!(check_file_permissions(user.as_ref(), argument_flags.interactive));
-        try!(check_htaccess_files(user.as_ref(), argument_flags.interactive));
+    if !(  suspended 
+         | inodes 
+         | permissions
+         | htaccess ) {
+        try!(check_suspension(user.as_ref()));
+        if interactive {
+            try!(check_inodes(user.as_ref(), true));
+            try!(check_file_permissions(user.as_ref(), true));
+            try!(check_htaccess_files(user.as_ref(), interactive));
+        }else{
+            {   let user = user.clone(); //increments the Arc counter.
+                children.push(thread::spawn(move || {
+                    check_inodes(user.as_ref(), false).unwrap();
+                }));
+            }{  let user = user.clone(); //increments the Arc counter.
+                children.push(thread::spawn(move || {
+                    check_file_permissions(user.as_ref(), false).unwrap();
+                }));
+            }{  let user = user.clone(); //increments the Arc counter.
+                children.push(thread::spawn(move || {
+                    check_htaccess_files(user.as_ref(), interactive).unwrap();
+                }));
+            }
+        }
     }
 
+    for child in children {
+        //wait for the thread to finish. returns a result.
+        let _ = child.join();
+    }
 
     Ok(())
 }
@@ -99,7 +142,7 @@ fn try_main() -> Result<(), Box<Error>> {
 
 
 fn check_file_permissions(user: &str, interactive: bool) -> Result<(), Box<Error>>{
-    println!("-- Checking for excessive file permissions...");
+    println!("-- {}Checking for excessive file permissions{}...", BLUE, NC);
 
     let mut homedir = PathBuf::from("/home/");
     homedir.push(user);
@@ -134,6 +177,7 @@ fn check_file_permissions(user: &str, interactive: bool) -> Result<(), Box<Error
     perms_vec.sort();
 
     if perms_vec.is_empty() {
+        println!("\n{}PERMISSIONS INFO{}:", GREEN, NC);
         println!("    No results found.");
     } else {
         if interactive {
@@ -162,6 +206,7 @@ fn check_file_permissions(user: &str, interactive: bool) -> Result<(), Box<Error
                 println!("    Perms: {}\tAmt Files: {}", last, count);
             }
         }else{
+            println!("\n{}PERMISSIONS INFO{}:", GREEN, NC);
             println!("    Total files found: {}", perms_vec.len());
             perms_vec.reverse();
             let mut count = 1;
@@ -187,14 +232,16 @@ fn check_file_permissions(user: &str, interactive: bool) -> Result<(), Box<Error
 }
 
 
-fn check_suspension(user: &str, interactive: bool) -> Result<(), Box<Error>>{
-    println!("-- Checking suspension status...");
+fn check_suspension(user: &str) -> Result<(), Box<Error>>{
+    println!("-- {}Checking suspension status{}...", BLUE, NC);
 
     let mut suspend_file_path = PathBuf::from("/var/cpanel/suspended/");
     suspend_file_path.push(user);
 
+    //I love this. basically, why check if the file exists when you can just try
+    //opening it? if you can't open it, then it doesn't exist! Much more elegant.
     match print_file(suspend_file_path.to_str().unwrap()) {
-        Ok(s) => println!("    {} is suspended: {}", user, s),
+        Ok(s) => println!("    {} is {}suspended{}. Reason: {}", user, RED, NC, s),
         Err(_) => println!("    {} is not suspended.", user),
     }
 
@@ -203,10 +250,12 @@ fn check_suspension(user: &str, interactive: bool) -> Result<(), Box<Error>>{
 }
 
 fn check_htaccess_files(user: &str, interactive: bool) -> Result<(), Box<Error>>{
-    println!("-- Checking for suPHP .htaccess conflicts...");
+    println!("-- {}Checking for suPHP .htaccess conflicts{}...", BLUE, NC);
 
     let mut homedir = PathBuf::from("/home/");
     homedir.push(user);
+
+    let mut hta_vec = Vec::<(String)>::new(); 
 
     for entry in WalkDir::new(&homedir) {
         let entry = match entry {
@@ -219,11 +268,21 @@ fn check_htaccess_files(user: &str, interactive: bool) -> Result<(), Box<Error>>
             let mut buffer = String::new();
             try!(f.read_to_string(&mut buffer));
             if re.is_match(buffer.as_ref()) {
-                println!("    -- {}", entry.path().display());
+                hta_vec.push(format!("    -- {}", entry.path().display()));
                 for cap in re.captures_iter(buffer.as_ref()).collect::<Vec<_>>(){
-                    println!("      {}", cap.name("val").unwrap());
+                    hta_vec.push(format!("      {}", cap.name("val").unwrap()));
                 }
             }
+        }
+    }
+
+    if !interactive { println!("\n{}HTACCESS INFO{}:", GREEN, NC); }
+
+    if hta_vec.is_empty() {
+        println!("    No results found.");
+    } else {
+        for item in hta_vec {
+            println!("{}", item);
         }
     }
 
@@ -231,14 +290,21 @@ fn check_htaccess_files(user: &str, interactive: bool) -> Result<(), Box<Error>>
 }
 
 fn check_inodes(user: &str, interactive: bool) -> Result<(), Box<Error>>{
-    println!("-- Gathering inode information...");
+    println!("-- {}Gathering inode information{}...", BLUE, NC);
 
     let mut homedir = PathBuf::from("/home/");
     homedir.push(user);
 
+    let total_inodes = try!(count_files(&homedir.as_path()));
+
     //not interactive, just give inodes.
     if !interactive {
-        println!("    {} -- {}", try!(count_files(&homedir.as_path())), homedir.display());
+        println!("\n{}INODE INFORMATION{}:", GREEN, NC);
+        if total_inodes >= 250000 {
+            println!("    {}{}{} -- {}", RED, total_inodes, NC, homedir.display());
+        } else {
+            println!("    {} -- {}", total_inodes, homedir.display());
+        }
     //interactive. what do you want to do?
     } else {
         //it's difficult to access the chars in a string, so we convert it to a Vec
@@ -265,6 +331,7 @@ fn check_inodes(user: &str, interactive: bool) -> Result<(), Box<Error>>{
             }
       
             if results.is_empty() {
+                println!("\n{}INODE INFORMATION{}:", GREEN, NC);
                 println!("    No results found.");
             } else {
                 results.sort();
@@ -285,7 +352,12 @@ fn check_inodes(user: &str, interactive: bool) -> Result<(), Box<Error>>{
             }
         //no breakdown. just print the inodes.
         } else {
-            println!("    {} -- {}", try!(count_files(&homedir.as_path())), homedir.display());
+            println!("\n{}INODE INFORMATION{}:", GREEN, NC);
+            if total_inodes >= 250000 {
+                println!("    {}{}{} -- {}", RED, total_inodes, NC, homedir.display());
+            } else {
+                println!("    {} -- {}", total_inodes, homedir.display());
+            }
         }
     }
 
@@ -329,30 +401,35 @@ fn save_to_file(dir: PathBuf, username: &str, extension:
 fn print_usage() {
     let usage = r#"
 -------------------------------------------------------------------------------
-Certified Hosting -- acctDiag -- Usage
+Certified Hosting -- acctdiag 1.2.0 -- Usage
 This tool is used to diagnose common issues with cPanel users. The program will 
-make no changes to accounts (unless using interactive mode, be careful!), but
-will report issues that it finds.
+make no changes to accounts (interactive mode may in the future, be careful!),
+but will report issues that it finds.
 
-This is still a work in progress. Issues may be found, features may be added.
-Any bugs, issues, or feature requests can be sent to andre@certifiedhosting.com.
+Threading support:
+This program will run the more hard working functions in their own threads
+(inodes/perms/htaccess) when *not* using interactive mode. This greatly reduces
+runtime. Please report any issues (formatting or otherwise) that you encounter.
 
 >>SYNTAX:
-[~]# acctDiag CPUSER <-i> <-n> <-p> <-h> <-s>
+[~]# acctdiag CPUSER <-i> <-n> <-p> <-h> <-s>
 *************
 CPUSER          :       Must be a valid cPanel user. MUST BE FIRST ARGUMENT.
 -i              :       INTERACTIVE flag. Setting this flag will allow you
-                        make runtime decisions. Try it out. MUST BE 2nd ARG.
+                        make runtime decisions. Try it out.
 *************
 -n              :       Check the amount of iNodes the account has.
 -p              :       Check file/directory Permissions.
--h              :       Check for issues with a .Htaccess file.
+-h              :       Check for issues with .Htaccess files.
 -s              :       Check if the account is Suspended.
 *************
-CPUSER must come first, -i must come second (optional).
-The rest may be in any order.
-You do not need to include any flags. By default, the program will have all
-flags set; setting one manually turns all others off.
+Except for the username, the rest of the options may be in any order.
+You do not need to include any flags. Running with no arguments would be the
+same as running `acctdiag user -n -p -h -s`.
+
+This is still a work in progress. Issues may be found, features may be added.
+Any bugs, problems, or feature requests can be sent to andre@certifiedhosting.com.
+
 -Andre
 -------------------------------------------------------------------------------
 "#;
