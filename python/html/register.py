de216f969b664e8efbe195513a2fc443a4e487c4
Andre Segura <andrensegura@gmail.com>
2015-11-26 05:41:08 -0500

    now handles email verification

--- python/html/register.py
+++ python/html/register.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 
-import mysql
+import mysql  #file i wrote
+import kcmail #file i wrote
+import re
+import random
 import cgi
 import cgitb; cgitb.enable() #for troubleshooting
 from passlib.hash import pbkdf2_sha256
@@ -13,17 +16,22 @@ def print_html_file(file_name):
 #CHECKS REGISTRATION INPUT CRED, RETURNS ok ON SUCCESS
 def register_user(user, email, passw, passw2):
     if not (user or email or passw or passw2):
-        return "sad"
+        return "nothing"
     result = mysql.execute_mysql("SELECT * FROM users WHERE username = '%s'" % (user))
     if not user or result: #no username give, or username exists
         return "user"
+    result = mysql.execute_mysql("SELECT * FROM users WHERE email = '%s'" % (email))
+    if not re.match(r"[^@]+@[^@]+\.[^@]+", email) or result:
+        return "email"
     if not passw or (passw != passw2) or (len(passw) < 8):
        return "password"
 
     pw_hash = pbkdf2_sha256.encrypt(passw, rounds=200000, salt_size=16)
-    mysql.execute_mysql( """INSERT INTO users (username, password, email)
-                          VALUES ('%s', '%s', '%s');"""
-                          % (user, pw_hash, email) )
+    ver_key = ''.join(random.choice('0123456789ABCDEF') for i in range(16))
+    mysql.execute_mysql( """INSERT INTO users (username, password, email, verified)
+                          VALUES ('%s', '%s', '%s', '%s');"""
+                          % (user, pw_hash, email, ver_key) )
+    kcmail.email_new_account(email, user, "http://keycellar.drago.ninja/register.cgi?verify=%s" % (ver_key))
     return "ok"
 
 #PRINT REGISTRATION FORM
@@ -32,19 +40,34 @@ def print_registration_form():
     print_html_file("/home/andre/domains/drago.ninja/header.html")
     print_html_file("register.html")
 
+def print_verification_success():
+    print "Content-type: text/html\n"
+    print_html_file("/home/andre/domains/drago.ninja/header.html")
+    print_html_file("verified.html")
+
 #GET VARIABLES
 form = cgi.FieldStorage()
 username = form.getvalue("username", "")
 email = form.getvalue("email", "")
 password = form.getvalue("password", "")
 password2 = form.getvalue("password2", "")
+verify = form.getvalue("verify", "")
 
 #DO STUFF WITH VARS
 #PRINT STUFF TO GET VARS
 r_result = register_user(username, email, password, password2)
+
+if verify:
+    mysql.execute_mysql("UPDATE users SET verified = '0' WHERE verified = '%s';" % (verify))
+    print_verification_success()
 if r_result == "ok":
     print "Location: http://keycellar.drago.ninja/success.html"
-print_registration_form()
+
+if not verify:
+    print_registration_form()
+
+if r_result == "email":
+    print "Invalid email address."
 if r_result == "password":
     print "Password mismatch or under 8 characters."
 elif r_result == "user":
