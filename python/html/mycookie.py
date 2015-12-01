4d3f435acdb168022f5cb7786363b1f815ecceed
Andre Segura <andrensegura@gmail.com>
2015-12-01 14:15:11 -0500

    first commit. handles getting the session

---
+++ python/html/mycookie.py
@@ -0,0 +1,12 @@
+import Cookie, os
+
+def get_cookie():
+    session = Cookie.SimpleCookie()
+    try:
+        session.load(os.environ["HTTP_COOKIE"])
+        if session["session"].value == "0":
+            session = ""
+    except ((Cookie.CookieError, KeyError)):
+        session = ""
+    return session
+
