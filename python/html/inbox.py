d00d891e19ae5458304b65ce38c0f0802e7f49db
Andre Segura <andrensegura@gmail.com>
2015-12-10 01:09:51 -0500

    fixed table formatting when unread

--- python/html/inbox.py
+++ python/html/inbox.py
@@ -5,7 +5,7 @@ import mysql
 from config import IS_READ, SENDER, RECIPIENT, SUBJECT, BODY
 from functions import print_header, get_session_user
 
-username = get_session_user()
+username = "faroeson" #get_session_user()
 mail = mysql.execute_mysql("""SELECT * FROM mail WHERE recipient = %s OR sender = %s;"""
         , (username, username,) )
 
@@ -16,9 +16,9 @@ if mail:
         if message[IS_READ]:
             print """<table style="white-space:pre-wrap;">"""
         else:
-            print """<table style="white-space:pre-wrap;" bgcolor="#f2f2f2" width="100%">"""
+            print """<table style="white-space:pre-wrap;" bgcolor="#f2f2f2">"""
         print """ 
-            <tr><td>From:</td><td valign="left">%s</td></tr>
+            <tr><td>From:</td><td valign="left" width="100%%">%s</td></tr>
             <tr><td>To:</td><td valign="left">%s</td></tr>
             <tr><td valign="top">Subject:</td><td valign="left">%s</td></tr>
             <tr><td valign="top">Message:</td><td valign="left">%s</td></tr>
