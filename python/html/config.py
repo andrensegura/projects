204e9fe23ea2abd28e1c7fb565d1545c27d9bcb2
Andre Segura <andrensegura@gmail.com>
2015-12-10 00:36:19 -0500

    added mail table

--- python/html/config.py
+++ python/html/config.py
@@ -1,15 +1,24 @@
 #####################
 #database structure
 #####################
-USERNAME=0
-PASSWORD=1
-EMAIL=2
-GAMES=3
-STEAM_PROFILE=4
-TRADES=5
-VERIFIED=6
-LOGGED_IN=7
-AVATAR=8
+#table: users
+USERNAME      = 0
+PASSWORD      = 1
+EMAIL         = 2
+GAMES         = 3
+STEAM_PROFILE = 4
+TRADES        = 5
+VERIFIED      = 6
+LOGGED_IN     = 7
+AVATAR        = 8
+
+#table: mail
+ID        = 0
+SENDER    = 1
+RECIPIENT = 2
+IS_READ   = 3
+SUBJECT   = 4
+BODY      = 5
 
 
 #####################
