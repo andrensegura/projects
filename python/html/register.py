d18cdac191e2b5bc1cd3eefe42d3ea59f67bdcf6
Andre Segura <andrensegura@gmail.com>
2015-12-13 12:48:05 -0500

    prettied up and added recovery method

--- python/html/register.py
+++ python/html/register.py
@@ -7,7 +7,6 @@ import re, string
 import random
 import cgi
 import cgitb; cgitb.enable() #for troubleshooting
-from passlib.hash import pbkdf2_sha256
 
 #CHECKS REGISTRATION INPUT CRED, RETURNS ok ON SUCCESS
 def register_user(user, email, passw, passw2):
@@ -31,6 +30,22 @@ def register_user(user, email, passw, passw2):
     kcmail.email_new_account(email, user, "http://keycellar.com/register.cgi?verify=%s" % (key))
     return "ok"
 
+def recover_password(action):
+    if action == "1":
+        rec_email = form.getvalue("rec_email", "")
+        result = mysql.execute_mysql("""SELECT email FROM users WHERE email=%s;""", (rec_email,) )
+        if not rec_email or not result:
+            print "Not a valid email address."
+        else:
+            print """Password recovery email sent."""
+            return 0
+    print """<h3>Password Recovery:</h3>
+            <form method="post" action="/register?recover=1">
+            Email address: <input type="text" name="rec_email">
+            <input type="submit" value="Send">
+            <form>
+          """
+
 #PRINT REGISTRATION FORM
 def print_registration_form():
     print_html_file("register.html")
@@ -47,6 +62,7 @@ email = form.getvalue("email", "")
 password = form.getvalue("password", "")
 password2 = form.getvalue("password2", "")
 verify = form.getvalue("verify", "")
+recover = form.getvalue("recover", "")
 
 #DO STUFF WITH VARS
 #PRINT STUFF TO GET VARS
@@ -54,18 +70,21 @@ r_result = register_user(username, email, password, password2)
 
 print_header()
 
-if verify:
+
+if recover:
+    recover_password(recover)
+elif verify:
     mysql.execute_mysql("""UPDATE users SET verified = '0' WHERE verified = %s;""" , (verify,))
     print_verification_success()
-if r_result == "ok":
-    print_registration_success()
+else:
+    if r_result == "ok":
+        print_registration_success()
+    elif not verify:
+        print_registration_form()
 
-if not verify:
-    print_registration_form()
-
-if r_result == "email":
-    print "Invalid email address."
-if r_result == "password":
-    print "Password mismatch or under 8 characters."
-elif r_result == "user":
-    print "Invalid username or username taken."
+    if r_result == "email":
+        print "Invalid email address."
+    if r_result == "password":
+        print "Password mismatch or under 8 characters."
+    if r_result == "user":
+        print "Invalid username or username taken."
