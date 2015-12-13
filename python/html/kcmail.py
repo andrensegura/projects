038df31187cc8b4a08e80aa42d4d75ebb4115915
Andre Segura <andrensegura@gmail.com>
2015-12-13 13:01:36 -0500

    added recovery email

--- python/html/kcmail.py
+++ python/html/kcmail.py
@@ -43,3 +43,36 @@ Andre Segura
        smtpObj.sendmail(sender, receivers, msg.as_string())         
     except SMTPException:
        print "Error: unable to send email"
+
+def email_password_recovery(address, key, recov_link)
+    sender = "andre@keycellar.com"
+    receivers = [address]
+    msg = MIMEText("""
+Hello,
+
+
+This email was sent to you because someone requested a password
+reset for the account using address "%s". If this was sent to you
+in error, you can disregard this message and no changes will be
+made.
+
+If, however, you <i>did</i> request a password reset, please click
+on the link below to begin. You will also need this key: %s
+%
+
+Thank you, and let me know if you have any trouble!
+
+
+Andre
+""" % (address, key, recov_link)
+
+    #the following three lines are to make sure that the email is RFC compliant.
+    msg['To'] = email.utils.formataddr(('', address))
+    msg['From'] = email.utils.formataddr(('Andre Segura', sender))
+    msg['Subject'] = 'KeyCellar: Password Recover Request'
+
+    try:
+       smtpObj = smtplib.SMTP('localhost')
+       smtpObj.sendmail(sender, receivers, msg.as_string())
+    except SMTPException:
+       print "Error: unable to send email"
